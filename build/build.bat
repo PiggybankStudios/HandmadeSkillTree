908ba67b31920ad453fa36923a4d877e0226c38f
PiggybankStudios <PrincessDungeonSmasher@gmail.com>
2024-09-22 08:46:16 -0700

    Switching over to Orca, using MinOrca repository as a template. Most of the code will be rebuilt from the ground up after this commit so definitely check prior commits for history about code before this point.

--- build/build.bat
+++ build/build.bat
@@ -1,12 +1,83 @@
 @echo off
 
-set BuildWindows=1
-set BuildLinux=0
+:: TODO: Do we need this?
+setlocal enabledelayedexpansion
 
-if "%BuildWindows%"=="1" (
-	build_win32.bat
+python --version 2>NUL
+if %ERRORLEVEL% NEQ 0 (
+	echo WARNING: Python isn't installed on this computer. Defines cannot be extracted from build_config.h!
+	exit /B %ERRORLEVEL%
 )
 
-if "%BuildLinux%"=="1" (
-	wsl ./build_linux.sh
+:: +--------------------------------------------------------------+
+:: |                      Paths and Options                       |
+:: +--------------------------------------------------------------+
+for /f %%i in ('orca sdk-path') do set OrcaSdkPath=%%i
+
+set SourceFolder=../src
+set DataFolder=../data
+set LibFolder=../lib
+set AppIconPath=%DataFolder%/icon.png
+set OutputWasmModulePath=module.wasm
+set MainSourcePath=%SourceFolder%/main.cpp
+set BuildConfigPath=%SourceFolder%/build_config.h
+set ExtractDefineScriptPath=%LibFolder%/include/gylib/ExtractDefine.py
+
+for /f "delims=" %%i in ('python %ExtractDefineScriptPath% %BuildConfigPath% PROJECT_NAME')      do set PROJECT_NAME=%%i
+for /f "delims=" %%i in ('python %ExtractDefineScriptPath% %BuildConfigPath% PROJECT_NAME_SAFE') do set PROJECT_NAME_SAFE=%%i
+for /f "delims=" %%i in ('python %ExtractDefineScriptPath% %BuildConfigPath% DEBUG_BUILD')       do set DEBUG_BUILD=%%i
+
+:: +--------------------------------------------------------------+
+:: |                  Compiler and Linker Flags                   |
+:: +--------------------------------------------------------------+
+:: Main Flags
+:: --target=wasm32 = Output to wasm32 binary
+:: -mbuild-memory = Something about bulk memory operations like memset being opt-in instructions in wasm, this is how we make those functions compile to single instructions
+:: -std=c++11 = ?
+:: -D__DEFINED_wchar_t = TODO: Why is this needed?? When compiling in C++ mode, we run into an error:
+::                             alltypes.h:116:13: error: cannot combine with previous 'int' declaration specifier
+set CompilerFlags=--target=wasm32 -mbulk-memory -std=c++11 -D__DEFINED_wchar_t
+:: Linker Flags
+:: -Wl,--no-entry = Allow no entry point in this compilation (Orca will act as the entry point, and it will use our wasm binary when we do orca bundle)
+:: -Wl,--export-dynamic = ?
+set CompilerFlags=%CompilerFlags% -Wl,--no-entry -Wl,--export-dynamic
+:: Include Directories
+:: --sysroot = Set the include directory for standard library headers (like stdint.h)
+:: -I = Add an include directory so search when resolving #include "..." lines
+set CompilerFlags=%CompilerFlags% --sysroot %OrcaSdkPath%/orca-libc -I%OrcaSdkPath%/src -I%OrcaSdkPath%/src/ext
+:: Linker Flags
+:: -L = Add a lib include folder
+:: -lorca_wasm = This is the precompiled binary that we compile with in order to get all the orca API functions exposed to us
+set LinkerFlags=-L %OrcaSdkPath%/bin -lorca_wasm
+
+if "%DEBUG_BUILD%"=="1" (
+	REM -g = Generate debug symbols
+	REM -O2 = Optimization level 2
+	set CompilerFlags=%CompilerFlags% -g -O0
+) else (
+	REM -O2 = Optimization level 2
+	set CompilerFlags=%CompilerFlags% -O2
 )
+
+:: +--------------------------------------------------------------+
+:: |                     Compile Wasm Module                      |
+:: +--------------------------------------------------------------+
+echo [Compiling WASM Module...]
+
+clang %CompilerFlags% %LinkerFlags% -o %OutputWasmModulePath% %MainSourcePath%
+
+IF %ERRORLEVEL% NEQ 0 (
+	echo [Compilation Failed!]
+	EXIT /B %ERRORLEVEL%
+) ELSE (
+	echo [Compilation Finished!]
+)
+
+:: +--------------------------------------------------------------+
+:: |                   Bundle Orca Application                    |
+:: +--------------------------------------------------------------+
+echo [Bunding Orca App...]
+
+orca bundle --name %PROJECT_NAME_SAFE% --icon %AppIconPath% --resource-dir %DataFolder% %OutputWasmModulePath%
+
+echo [Finished Bundling!]
---
+++ src/main.cpp
@@ -0,0 +1,94 @@
+/*
+File:   main.cpp
+Author: Taylor Robbins
+Date:   09\21\2024
+Description: 
+	** This is the main file that gets compiled into the WASM module.
+	** All other files are #included from this one (including cpp files!) to keep compilation simple
+*/
+
+#include <math.h>
+#include <orca.h>
+
+#include "orca_aliases.h"
+
+#include "build_config.h"
+
+OC_Surface_t surface;
+OC_CanvasRenderer_t renderer;
+OC_CanvasContext_t canvasContext;
+v2 mousePos;
+v2 ScreenSize;
+v2i ScreenSizei;
+rec ScreenRec;
+OC_Image_t pigTexture;
+
+EXPORT void OC_OnInit()
+{
+	OC_Log_I("OC_OnInit!");
+	
+	OC_WindowSetTitle(NewStr(PROJECT_NAME_STR));
+	
+	renderer = OC_CanvasRendererCreate();
+	surface = OC_CanvasSurfaceCreate(renderer);
+	canvasContext = OC_CanvasContextCreate();
+	
+	pigTexture = OC_ImageCreateFromPath(renderer, NewStr("Resources/Textures/pig_invalid.png"), false);
+	OC_Assert(!OC_ImageIsNil(pigTexture), "Failed to load pig_invalid.png!");
+}
+
+EXPORT void OC_OnResize(u32 width, u32 height)
+{
+	// OC_Log_I("OC_OnResize(%d, %d)!", width, height);
+	ScreenSize = NewVec2((r32)width, (r32)height);
+	ScreenSizei = NewVec2i((i32)width, (i32)height);
+	ScreenRec = NewRec(0, 0, (r32)width, (r32)height);
+}
+
+EXPORT void OC_OnKeyDown(OC_ScanCode_t scan, OC_KeyCode_t key)
+{
+	//TODO: Implement me!
+}
+
+EXPORT void OC_OnKeyUp(OC_ScanCode_t scan, OC_KeyCode_t key)
+{
+	//TODO: Implement me!
+}
+
+EXPORT void OC_OnMouseMove(float x, float y, float dx, float dy)
+{
+	mousePos.x = x;
+	mousePos.y = y;
+}
+
+EXPORT void OC_OnFrameRefresh()
+{
+	OC_ArenaScope_t scratch = OC_ScratchBegin();
+	
+	OC_CanvasContextSelect(canvasContext);
+	OC_SetColorRgba(NewColorfHex(0xFFCC3B95));
+	OC_Clear();
+	
+	OC_SetColorRgba(NewColorfHex(0xFF222222));
+	// OC_ImageDraw(pigTexture, ScreenRec); //TODO: Color doesn't matter for OC_ImageDraw?
+	OC_SetImage(pigTexture);
+	OC_RectangleFill(ScreenRec);
+	
+	OC_SetColorRgba(NewColorfHex(0xFFF27CB1));
+	OC_SetImage(pigTexture);
+	OC_RoundedRectangleFill(NewRec(mousePos.x, mousePos.y, 100, 200), 25);
+	OC_SetImage(OC_ImageNil());
+	
+	OC_CircleFill(mousePos.x - 50 + 25, mousePos.y - 50 + 25, 50);
+	
+	OC_SetColorRgba(NewColorfHex(0xFFF83333));
+	OC_SetWidth(20);
+	OC_MoveTo(100, 100);
+	OC_CubicTo(100, 100, mousePos.x, mousePos.y, 200, 100);
+	OC_Stroke();
+	
+	OC_CanvasRender(renderer, canvasContext, surface);
+	OC_CanvasPresent(renderer, surface);
+	
+	OC_ScratchEnd(scratch);
+}
